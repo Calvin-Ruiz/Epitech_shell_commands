d25d4a6cf33e4e2952bcf2d161c294a29515f032
Calvin Ruiz <43058983+Calvin-Ruiz@users.noreply.github.com>
2019-10-05 13:43:18 +0200

    Add files via upload

--- Smkfile.sh
+++ Smkfile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd "tests"
-echo -e "/*\n** EPITECH PROJECT, 2019\n** C Pool Day $1\n** File description:\n** $2.c\n*/\n\nint $2(void)\n{\n    \n}" > "$2.c"
+echo -e "/*\n** EPITECH PROJECT, 2019\n** C Pool $1 2019\n** File description:\n** $2.c\n*/\n\nint $2(void)\n{\n    \n}" > "$2.c"
