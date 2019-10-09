b190a926875263f9191226b9a9cb0b3e05204aa3
Calvin Ruiz <43058983+Calvin-Ruiz@users.noreply.github.com>
2019-10-09 17:40:35 +0200

    Add files via upload

--- mkfile.sh
+++ mkfile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "tests"
-echo -e "/*\n** EPITECH PROJECT, 2019\n** C Pool Day $1\n** File description:\n** $2.c\n*/\n\nint $2(void)\n{\n    \n}" > "$2.c"
+echo -e "/*\n** EPITECH PROJECT, 2019\n** C Pool Day $1\n** File description:\n** $2.c\n*/\n#include <stdlib.h>\n#include <unistd.h>\n\nint $2(void)\n{\n    \n}" > "$2.c"
 emacs -nw "$2.c"
