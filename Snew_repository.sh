e19b89768b895fb9918942951eabcef70c50e244
Calvin Ruiz <43058983+Calvin-Ruiz@users.noreply.github.com>
2019-10-09 09:29:41 +0200

    Delete Snew_repository.sh

--- Snew_repository.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-blih -u calvin.ruiz@epitech.eu repository create "CPool_$1_2019"
-blih -u calvin.ruiz@epitech.eu repository setacl "CPool_$1_2019" ramassage-tek r
-git clone "git@git.epitech.eu:/calvin.ruiz@epitech.eu/CPool_$1_2019"
-cp tests/lib/my/* "CPool_$1_2019/lib/my"
