6bc2cf251c0450a82ae5e60ade976224eab0f552
Calvin Ruiz <43058983+Calvin-Ruiz@users.noreply.github.com>
2019-10-10 10:55:56 +0200

    Add files via upload

--- Shappend.sh
+++ Shappend.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-mkdir "CPool_$1_2019/include"
 cd "CPool_$1_2019/include"
-cp -f "../tests/include/$2" "$2"
+cp -f "../../tests/include/$2" "$2"
 git add --all
 git commit -m "auto-commit $2"
 git push origin master
