2a74feeaeac64d3329314fe0fe26dbdb08d2108f
Calvin Ruiz <43058983+Calvin-Ruiz@users.noreply.github.com>
2019-10-10 10:12:03 +0200

    Add files via upload

--- hmkfile.sh
+++ hmkfile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "headers"
-echo -e "/*\n** EPITECH PROJECT, 2019\n** C Pool Day $1\n** File description:\n** $2.c\n*/\n" > "$2.h"
+echo -e "/*\n** EPITECH PROJECT, 2019\n** C Pool Day $1\n** File description:\n** $2.h\n*/\n" > "$2.h"
 emacs -nw "$2.h"
