c6d98c9afe0a9eb207b341e4e7b3d194b2ea22ba
Calvin Ruiz <43058983+Calvin-Ruiz@users.noreply.github.com>
2019-10-10 19:31:15 +0200

    Delete update.sh

--- update.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/Calvin-Ruiz/Epitech_shell_commands.git
-cp -r -f Epitech_shell_commands/* "./"
-chmod -c 755 installer.sh
-./installer.sh "$1"
-rm -r -f Epitech_shell_commands
