7dcda26beeadcfa7fe7c687a864d4e7ba23c09bc
Calvin Ruiz <43058983+Calvin-Ruiz@users.noreply.github.com>
2019-10-11 11:03:47 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-echo -e "#!/bin/bash
-echo \"------- Création du répertoire n°\$1 -------\"
+echo -e "echo \"------- Création du répertoire n°\$1 -------\"
 blih -u $1 repository create \"CPool_Day\$1_2019\"
 blih -u $1 repository setacl \"CPool_Day\$1_2019\" ramassage-tek r
 git clone \"git@git.epitech.eu:/$1/CPool_Day\$1_2019\"\nmkdir \"CPool_Day\$1_2019/include\"
 mkdir -p \"CPool_Day\$1_2019/lib/my\"\ncp tests/lib/my/* \"CPool_Day\$1_2019/lib/my\"
 echo \"------- Auto-update des outils -------\"
 ./update.sh" > new_repository.sh
-echo -e "#!bin/bash
-blih -u $1 repository create \"CPool_\$1_2019\"\nblih -u $1 repository setacl \"CPool_\$1_2019\" ramassage-tek r
-git clone \"git@git.epitech.eu:/$1/CPool_\$1_2019\"\nmkdir \"CPool_\$1_2019/include\"\nmkdir -p \"CPool_\$1_2019/lib/my\"
+echo -e "blih -u $1 repository create \"CPool_\$1_2019\"\nblih -u $1 repository setacl \"CPool_\$1_2019\" ramassage-tek r
+git clone \"git@git.epitech.eu:/$1/CPool_\$1_2019\"\nmkdir \"CPool_\$1_2019/include\"
+mkdir -p \"CPool_\$1_2019/lib/my\"
 cp tests/lib/my/* \"CPool_\$1_2019/lib/my\"" > Snew_repository.sh
 echo -e -n "git clone https://github.com/Calvin-Ruiz/Epitech_shell_commands.git
 mv -f Epitech_shell_commands/* \"./\"
