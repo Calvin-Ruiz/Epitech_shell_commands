2b361b51053af24932db2bafc3f58ef5abf5aca3
Calvin Ruiz <43058983+Calvin-Ruiz@users.noreply.github.com>
2020-01-27 10:33:56 +0100

    Delete Shappend.sh

--- Shappend.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd "CPool_$1_2019/include"
-cp -f "../../tests/include/$2" "$2"
-git add --all
-git commit -m "auto-commit $2"
-git push origin master
-cd ../..
-echo -n -e "| grep --invert-match $2.h " >> appened
