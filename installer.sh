5facb7fa8207b51869d17eef3cb1025a72490195
Calvin Ruiz <43058983+Calvin-Ruiz@users.noreply.github.com>
2020-01-27 13:07:35 +0100

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -7,7 +7,7 @@ blih -u $1 repository setacl \"\$1\" ramassage-tek r
 git clone \"git@git.epitech.eu:/$1/\$1\"
 cd \"\$1\"
 mkdir \"include\"
-echo -e \"/*\n** EPITECH PROJECT, 2019\n** $1\n** File description:\n** main.c\n*/\n#include <stdlib.h>\n#include <unistd.h>\n#include <main.h>\n\nint main(int ac, char **av)\n{\n    \n}\" > \"main.c\"
+echo -e \"/*\n** EPITECH PROJECT, 2019\n** \$1\n** File description:\n** main.c\n*/\n#include <stdlib.h>\n#include <unistd.h>\n#include <main.h>\n\nint main(int ac, char **av)\n{\n    \n}\" > \"main.c\"
 hmkfile \"\$1\" \"main\"
 mmkfile \"\$1\" \"\$2\" \"\$3 -I include\"
 echo -e \".gitignore\n\$2\nvgcore*\" > .gitignore
