6ae5cef31ba1de8f5a8e40644c3ad6a9d9e95477
Calvin Ruiz <43058983+Calvin-Ruiz@users.noreply.github.com>
2020-02-17 12:53:31 +0100

    Fix new_repository

--- installer.sh
+++ installer.sh
@@ -9,8 +9,8 @@ cd \"\$1\"
 mkdir \"include\"
 echo -e \"/*\n** EPITECH PROJECT, 2019\n** \$1\n** File description:\n** main.c\n*/\n#include <stdlib.h>\n#include <unistd.h>\n#include <main.h>\n\nint main(int ac, char **av)\n{\n    \n}\" > \"main.c\"
 cp -r ~/Repository_bases/* .
-hmkfile \"\$1\" \"main\"
-mmkfile \"\$1\" \"\$2\" \"\$3 -I include\"
+hmkfile \"main\"
+mmkfile \"\$2\" \"\$3 -I include\"
 echo -e \".gitignore\n\$2\nvgcore*\" > .gitignore
 rm -f *~
 git add --all
