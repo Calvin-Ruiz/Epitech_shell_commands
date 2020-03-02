202951e0f051dafc363be2d237fedefb9bb2e03e
Calvin Ruiz <calvin.ruiz@epitech.eu>
2020-03-02 14:22:56 +0100

    Update 1/2

--- installer.sh
+++ installer.sh
@@ -2,8 +2,8 @@ mkdir ~/bin
 chmod 755 bin/*
 mv -f bin/* ~/bin
 cd ~/bin
-echo "blih -u $1 repository create \"\$1\"
-blih -u $1 repository setacl \"\$1\" ramassage-tek r
+echo "blih -u $1 repository create \"\$1\" || blih -u $1 repository create \"\$1\"
+blih -u $1 repository setacl \"\$1\" ramassage-tek r || blih -u $1 repository setacl \"\$1\" ramassage-tek r
 git clone \"git@git.epitech.eu:/$1/\$1\"
 cd \"\$1\"
 mkdir \"include\"
@@ -15,7 +15,7 @@ echo -e \".gitignore\n\$2\nvgcore*\" > .gitignore
 rm -f *~
 git add --all
 git commit -m \"create repository base\"
-git push origin master
+git push origin master &
 atom . || code ." > new_repository
 echo "git clone git@git.epitech.eu:/$1/\$1" > my_clone
 echo -n "blih -u $1 repository setacl \"\$(get_highest_level \$PWD)\" \"\$1\" rw" > add_user
@@ -24,5 +24,4 @@ chmod 777 Epitech_shell_commands/installer.sh
 Epitech_shell_commands/installer.sh $1
 rm -r -f Epitech_shell_commands" > update_shells
 
-chmod 755 new_repository my_clone add_user
-
+chmod 755 new_repository my_clone add_user update_shells
